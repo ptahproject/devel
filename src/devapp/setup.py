341dd43e66d494991f4f64ad8a185d5726b57c40
Rajiv Bakulesh Shah <raj@raj-desktop.(none)>
2011-09-26 12:22:02 -0500

    formatted code

--- src/devapp/setup.py
+++ src/devapp/setup.py
@@ -1,24 +1,28 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 
 requires = [
     'ptah',
     'ptah_cms',
     'ptah_app',
-    ]
+]
 
-setup(name='devapp',
-      version='0.0',
-      description='experimental package',
-      classifiers=[
+
+setup(
+    name='devapp',
+    version='0.0',
+    description='experimental package',
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pylons",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires = requires,
-      entry_points = {
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires = requires,
+    entry_points = {
         'memphis': ['package = devapp'],
-        },
-      )
+    },
+)
