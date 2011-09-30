66205dab273c2fc0224729a617ea36d7298723dc
Nikolay Kim <fafhrd91@gmail.com>
2011-09-30 10:47:44 -0500

    update

--- src/devsocket/devsocket/server.py
+++ src/devsocket/devsocket/server.py
@@ -1,26 +1,3 @@
-#-----------------------------------------------------------------------------#
-#   server.py                                                                 #
-#                                                                             #
-#   Copyright (c) 2011, Enfold Systems, Inc.                                  #
-#   All rights reserved.                                                      #
-#                                                                             #
-#       Authors:                                                              #
-#       Raj Shah (raj@enfoldsystems.com)                                      #
-#                                                                             #
-#           This software is licensed under the Terms and Conditions          #
-#           contained within the "LICENSE.txt" file that accompanied          #
-#           this software.  Any inquiries concerning the scope or             #
-#           enforceability of the license should be addressed to:             #
-#                                                                             #
-#               Enfold Systems, Inc.                                          #
-#               4617 Montrose Blvd., Suite C215                               #
-#               Houston, Texas 77006 USA                                      #
-#               p. +1 713.942.2377 | f. +1 832.201.8856                       #
-#               www.enfoldsystems.com                                         #
-#               info@enfoldsystems.com                                        #
-#-----------------------------------------------------------------------------#
-
-
 import gevent.monkey
 gevent.monkey.patch_all()
 
--- src/devsocket/devsocket/views.py
+++ src/devsocket/devsocket/views.py
@@ -1,26 +1,3 @@
-#-----------------------------------------------------------------------------#
-#   views.py                                                                  #
-#                                                                             #
-#   Copyright (c) 2011, Enfold Systems, Inc.                                  #
-#   All rights reserved.                                                      #
-#                                                                             #
-#       Authors:                                                              #
-#       Raj Shah (raj@enfoldsystems.com)                                      #
-#                                                                             #
-#           This software is licensed under the Terms and Conditions          #
-#           contained within the "LICENSE.txt" file that accompanied          #
-#           this software.  Any inquiries concerning the scope or             #
-#           enforceability of the license should be addressed to:             #
-#                                                                             #
-#               Enfold Systems, Inc.                                          #
-#               4617 Montrose Blvd., Suite C215                               #
-#               Houston, Texas 77006 USA                                      #
-#               p. +1 713.942.2377 | f. +1 832.201.8856                       #
-#               www.enfoldsystems.com                                         #
-#               info@enfoldsystems.com                                        #
-#-----------------------------------------------------------------------------#
-
-
 import gevent
 
 from pyramid.renderers import render_to_response
@@ -55,6 +32,7 @@ class ConnectIOContext(SocketIOContext):
             index = len(_messages)
             while self.io.connected():
                 while index < len(_messages):
+                    print 'send message:', _messages[index]
                     self.msg('message', message=_messages[index])
                     index += 1
                 gevent.sleep(0.5)
