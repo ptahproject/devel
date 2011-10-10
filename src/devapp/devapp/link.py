678b67169212ff46e96d886daf17fe021cb8059f
Nikolay Kim <fafhrd91@gmail.com>
2011-10-10 14:12:20 -0500

    fix for link

--- src/devapp/devapp/link.py
+++ src/devapp/devapp/link.py
@@ -1,13 +1,15 @@
 import sqlalchemy as sqla
-from pyramid.httpexceptions import HTTPFound
+import ptah, ptah_cms
 from memphis import view, form
-from ptah import checkPermission
-import ptah_cms
+from pyramid.httpexceptions import HTTPFound
+
 
 class Link(ptah_cms.Content):
     __tablename__ = 'ptah_cms_link'
     __type__ = ptah_cms.Type('link', permission=ptah_cms.AddContent)
+
     href = sqla.Column(sqla.Unicode)
+
 
 @view.pyramidView(context=Link, permission=ptah_cms.View)
 def link_view(context, request):
@@ -15,11 +17,16 @@ def link_view(context, request):
         If you have permission to edit it it will display the form.
         If you do not have ability to edit it; you will be redirected.
     """
-    can_edit = checkPermission(ptah_cms.ModifyContent, context, throw=False)
+    can_edit = ptah.checkPermission(
+        ptah_cms.ModifyContent, context, throw=False)
 
     if can_edit:
         vform = form.DisplayForm(context, request) # needs better UI
         vform.fields = Link.__type__.fieldset
+        vform.content = {
+            'title': context.title,
+            'description': context.description,
+            'href': context.href}
         vform.update()
         # the below render() would display form html without enclosing layout
         #return vform.render()
@@ -29,7 +36,7 @@ def link_view(context, request):
         The layout is the "wrapping HTML" e.g. ptah_app layout you
         see at http://localhost:8080/
         """
-        layout = view.queryLayout(context, request, 'page')
+        layout = view.queryLayout(request, context)
         return layout(vform.render())
 
     raise HTTPFound(location=context.href)
