8c3624df45d3af19abaf234e7f682b035c3a3aea
Nikolay Kim <fafhrd91@gmail.com>
2012-03-01 16:12:34 -0600

    cleanup

--- src/devsocket/devsocket/msg.py
+++
@@ -1,9 +0,0 @@
-import ptah
-import logging
-
-logger = logging.getLogger('devsocket')
-
-
-@ptah.subscriber(ptah.events.ContentEvent)
-def contentEvent(ev):
-        MSG.send('%s: %s'%(ev.__class__.__name__, ev.object.title))
